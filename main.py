b3fbafc201c7152edb8bf8fecba463e3c9a641ed
kartheeswaran <kartheeswaran07v@gmail.com>
2024-01-28 01:04:33 +0530

    pqww

--- main.py
+++ main.py
@@ -5827,6 +5827,7 @@ def DATA_UPLOAD_BULK():
         # add_many(getRowsFromCsvFile("csv/positioner.csv"), positioner)
         # add_many(getRowsFromCsvFile("csv/limit_switch.csv"), limitSwitch)
         # add_many(getRowsFromCsvFile("csv/solenoid.csv"), solenoid)
+        pass
 
 # DATA_UPLOAD_BULK()
 # cv_upload(getRowsFromCsvFile("csv/cvtable.csv"))
